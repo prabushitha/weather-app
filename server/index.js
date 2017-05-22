b0bf3ba847a6f1ca7090c5f7927c93c9ee36f1d8
prabushitha <prabushitha@gmail.com>
2017-05-22 13:28:58 +0000

    node server init

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
+var http = require('http');
 var bodyParser = require('body-parser');
-var app = require('express');
+var express = require('express');
+var app = express();
+var API_KEY_WEATHER = "44407d70b194664b59ecf42a7d7d238c";
 //using body parser
 app.use(bodyParser.json());
 
@@ -16,5 +19,50 @@ app.use(function (req, res, next) {
 app.get('/',function(req,res){
 	res.send("Welcome to music app");
 });
+
+app.get('/weather/:_city',function(req,res){
+
+  var city = req.params._city;
+  console.log(city);
+  //var query = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid='+API_KEY_WEATHER;
+  var host = 'api.openweathermap.org';
+  var path = '/data/2.5/weather?q='+city+'&units=metric&appid='+API_KEY_WEATHER;
+
+  var options = {
+    host : host,
+    port : 80,
+    path : path,
+    method : 'GET'
+  };
+  var request = http.request(options, function (response){
+    var body = "";
+
+    // Read the data
+    response.on('data', function (chunk){
+      body += chunk;
+    });
+
+    response.on('end', function(){
+      if (response.statusCode === 200) {
+        try {
+          // Parse the data
+          var forecast = JSON.parse(body);
+          res.json(forecast);
+        } catch(error) {
+          console.log(error);
+        }
+      } else {
+        console.log('There was an error');
+      }
+    });
+  });
+  request.end();
+  request.on('error', function(e) {
+    console.error(e);
+  });
+});
+
+
+
 app.listen(3000);
 console.log('Running on port 3000');
