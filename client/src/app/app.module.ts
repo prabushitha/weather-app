9d4c41bcd7344864a7d5f6a5ec4a7d98a85e0ccc
prabushitha <prabushitha@gmail.com>
2017-05-25 14:14:57 +0000

    search component added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 //google maps using angular maps
@@ -11,19 +11,24 @@ import { AppComponent } from './components/main/app.component';
 import { MapComponent } from './components/map/map.component';
 import { CityComponent } from './components/city/city.component';
 import { WeatherComponent } from './components/weather/weather.component';
+import { SearchComponent } from './components/search/search.component';
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
     CityComponent,
-    WeatherComponent
+    WeatherComponent,
+    SearchComponent
   ],
   imports: [
     AgmCoreModule.forRoot({
-      apiKey: "AIzaSyAshN3KKFFMBQYr_kRUDOES0MFfm5X-xqY"
+      //apiKey: "AIzaSyAshN3KKFFMBQYr_kRUDOES0MFfm5X-xqY"
+      apiKey:"AIzaSyBPKyiDWDZrgBz_C0g_lTpuJQv7K18mbF0",
+      libraries: ["places"]
     }),
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
   providers: [],
---
+++ client/src/app/components/search/search.component.ts
@@ -0,0 +1,60 @@
+import { Component, Input,Output, EventEmitter, NgZone, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { AgmCoreModule, MouseEvent, MapsAPILoader } from 'angular2-google-maps/core';
+import {City} from '../../models/city';
+
+declare let google: any;
+
+@Component({
+  selector: 'search',
+  templateUrl: '../search/search.html',
+  styleUrls: ['../search/search.css'],
+
+})
+
+export class SearchComponent {
+  @ViewChild("search") public searchElementRef: ElementRef;
+  @Output() onresult:EventEmitter<any> = new EventEmitter();;
+  public searchControl: FormControl;
+  constructor(private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone){
+
+    }
+    ngOnInit() {
+
+      //create search FormControl
+      this.searchControl = new FormControl();
+
+      //load Places Autocomplete
+      this.mapsAPILoader.load().then(() => {
+        let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+          types: ["(cities)"]
+        });
+
+        autocomplete.addListener("place_changed", () => {
+          let self = this;
+          this.ngZone.run(() => {
+            //get the place result
+            let place = autocomplete.getPlace();
+
+            //verify result
+            if (place.geometry === undefined || place.geometry === null) {
+              return;
+            }
+            //console.log(self);
+            let result = {coords:{
+              latitude:place.geometry.location.lat(),
+              longitude:place.geometry.location.lng()
+            }};
+            self.onresult.emit(result);
+            //set latitude, longitude and zoom
+            //this.latitude = place.geometry.location.lat();
+            //this.longitude = place.geometry.location.lng();
+            //this.zoom = 12;
+          });
+        });
+      });
+    }
+
+}
